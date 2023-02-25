3ff102dee69d5f35672f1c9249d2246e8ed5478b
A-Kireche <la_kireche@esi.dz>
2023-02-25 05:09:09 +0100

    payement

--- lib/page-7/payement.dart
+++ lib/page-7/payement.dart
@@ -7,7 +7,7 @@ class Wallet extends StatelessWidget {
   Widget build(BuildContext context) {
     double baseWidth = 390;
     double fem = MediaQuery.of(context).size.width / baseWidth;
-    double ffem = fem * 0.97;
+    double ffem = fem *0.97;
     return Container(
       width: double.infinity,
       child: Container(
