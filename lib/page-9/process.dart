47c7ec3c94a0566c1b2d9cdcffee2146d95db04e
A-Kireche <la_kireche@esi.dz>
2023-02-25 05:09:59 +0100

    loading

---
+++ lib/page-9/process.dart
@@ -0,0 +1,303 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 390;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return Container(
+      width: double.infinity,
+      child: Container(
+        // hometransportation1oVf (1:17)
+        width: double.infinity,
+        decoration: BoxDecoration (
+          color: Color(0xffffffff),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              // iphonexstatusbarsstatusbarQtm (1:18)
+              width: double.infinity,
+              height: 44*fem,
+              child: Container(
+                // iphonexstatusbarsstatusbarblac (1:20)
+                padding: EdgeInsets.fromLTRB(35.5*fem, 16*fem, 29.26*fem, 10*fem),
+                width: double.infinity,
+                height: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      // timeLvq (1:36)
+                      margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 230.91*fem, 0*fem),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          style: SafeGoogleFont (
+                            'Gudea',
+                            fontSize: 14*ffem,
+                            fontWeight: FontWeight.w700,
+                            height: 1.2359999248*ffem/fem,
+                            letterSpacing: -0.2800000012*fem,
+                            color: Color(0xff000000),
+                          ),
+                          children: [
+                            TextSpan(
+                              text: '9:4',
+                              style: SafeGoogleFont (
+                                'Gudea',
+                                fontSize: 14*ffem,
+                                fontWeight: FontWeight.w700,
+                                height: 1.2375*ffem/fem,
+                                letterSpacing: -0.2800000012*fem,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                            TextSpan(
+                              text: '1',
+                              style: SafeGoogleFont (
+                                'Gudea',
+                                fontSize: 14*ffem,
+                                fontWeight: FontWeight.w700,
+                                height: 1.2375*ffem/fem,
+                                letterSpacing: -0.2800000012*fem,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // cellularconnectionJRj (1:30)
+                      margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 5.23*fem, 4*fem),
+                      width: 17.68*fem,
+                      height: 10.67*fem,
+                      child: Image.asset(
+                        'assets/page-1/images/cellular-connection.png',
+                        width: 17.68*fem,
+                        height: 10.67*fem,
+                      ),
+                    ),
+                    Container(
+                      // wifixWH (1:26)
+                      margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 5.24*fem, 4.37*fem),
+                      width: 15.88*fem,
+                      height: 10.97*fem,
+                      child: Image.asset(
+                        'assets/page-1/images/wifi.png',
+                        width: 15.88*fem,
+                        height: 10.97*fem,
+                      ),
+                    ),
+                    Container(
+                      // batteryEyb (1:22)
+                      margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 4*fem),
+                      width: 25.3*fem,
+                      height: 11.33*fem,
+                      child: Image.asset(
+                        'assets/page-1/images/battery.png',
+                        width: 25.3*fem,
+                        height: 11.33*fem,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              // autogroupo9suKVF (TZZFCnpDnRygNLg4mso9Su)
+              padding: EdgeInsets.fromLTRB(38.03*fem, 99*fem, 33.2*fem, 118.52*fem),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    // autogroupw9xtPzu (TZZEEEc86rFAW1q2wRW9XT)
+                    margin: EdgeInsets.fromLTRB(2.26*fem, 0*fem, 0*fem, 17.16*fem),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          // linecharttRs (1:39)
+                          margin: EdgeInsets.fromLTRB(0*fem, 1*fem, 56.86*fem, 0*fem),
+                          width: 67.14*fem,
+                          height: 67.14*fem,
+                          child: Image.asset(
+                            'assets/page-1/images/line-chart.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          // linechartN69 (1:40)
+                          margin: EdgeInsets.fromLTRB(0*fem, 68*fem, 55.05*fem, 0*fem),
+                          width: 67.95*fem,
+                          height: 67.95*fem,
+                          child: Image.asset(
+                            'assets/page-1/images/line-chart-nGm.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          // barchart3xy (1:37)
+                          width: 68.08*fem,
+                          height: 68.08*fem,
+                          child: Image.asset(
+                            'assets/page-1/images/bar-chart.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    // autogroupcproy5w (TZZEVK1LTubitjvb2ucPRo)
+                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 109.6*fem),
+                    width: double.infinity,
+                    height: 172.88*fem,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          // linechart14dB (1:43)
+                          margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 2.05*fem, 0*fem),
+                          width: 68.92*fem,
+                          height: 67.94*fem,
+                          child: Image.asset(
+                            'assets/page-1/images/line-chart-1-Zp9.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          // autogroupuucmxyT (TZZEgPXDCwRWoeeUpGuUCM)
+                          margin: EdgeInsets.fromLTRB(0*fem, 0.15*fem, 0*fem, 0*fem),
+                          width: 247.8*fem,
+                          height: double.infinity,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                // linechart1fcy (1:42)
+                                left: 179.1767578125*fem,
+                                top: 0*fem,
+                                child: Align(
+                                  child: SizedBox(
+                                    width: 68.62*fem,
+                                    height: 67.63*fem,
+                                    child: Image.asset(
+                                      'assets/page-1/images/line-chart-1.png',
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                // group16k8d (1:45)
+                                left: 0*fem,
+                                top: 33.7295227051*fem,
+                                child: Container(
+                                  width: 180*fem,
+                                  height: 139*fem,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        // ellipse20F5P (1:46)
+                                        margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 8*fem, 10*fem),
+                                        child: TextButton(
+                                          onPressed: () {},
+                                          style: TextButton.styleFrom (
+                                            padding: EdgeInsets.zero,
+                                          ),
+                                          child: Container(
+                                            width: 74*fem,
+                                            height: 74*fem,
+                                            child: Image.asset(
+                                              'assets/page-1/images/ellipse-20.png',
+                                              width: 74*fem,
+                                              height: 74*fem,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        // yourdataisbeingprocessedHGy (1:47)
+                                        constraints: BoxConstraints (
+                                          maxWidth: 180*fem,
+                                        ),
+                                        child: Text(
+                                          'Your data is being processed',
+                                          textAlign: TextAlign.center,
+                                          style: SafeGoogleFont (
+                                            'Open Sans',
+                                            fontSize: 20*ffem,
+                                            fontWeight: FontWeight.w700,
+                                            height: 1.3625*ffem/fem,
+                                            color: Color(0xff686767),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    // autogroupgu5s8oP (TZZF1NyZuZhA5nAsJLGU5s)
+                    margin: EdgeInsets.fromLTRB(18.71*fem, 0*fem, 22.55*fem, 11.05*fem),
+                    width: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Container(
+                          // barchart1eFw (1:44)
+                          margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 142.65*fem, 0*fem),
+                          width: 67.21*fem,
+                          height: 67.21*fem,
+                          child: Image.asset(
+                            'assets/page-1/images/bar-chart-1.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          // linechartLeZ (1:41)
+                          margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 0.7*fem),
+                          width: 67.65*fem,
+                          height: 67.65*fem,
+                          child: Image.asset(
+                            'assets/page-1/images/line-chart-M3b.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    // barchartyhX (1:38)
+                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 5.34*fem, 0*fem),
+                    width: 67.48*fem,
+                    height: 67.48*fem,
+                    child: Image.asset(
+                      'assets/page-1/images/bar-chart-K4Z.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+          );
+  }
+}
